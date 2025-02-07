00d4bd144b6df18deefe32439fc7ab7c99ec0fa9
Dominique YOLIN <d.yolin@isartdigital.com>
2025-02-07 15:28:09 +0100

    Change background color (end of exo 1)

--- app/src/main/java/com/arcreane/webstartdroid/ExoActivity.java
+++ app/src/main/java/com/arcreane/webstartdroid/ExoActivity.java
@@ -1,9 +1,12 @@
 package com.arcreane.webstartdroid;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +18,8 @@ public class ExoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_exo);
+        RelativeLayout main = findViewById(R.id.main);
+        main.setBackgroundColor(ContextCompat.getColor(this,( R.color.other)));
 
     }
 }
